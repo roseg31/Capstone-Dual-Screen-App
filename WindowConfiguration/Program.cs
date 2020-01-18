8128cf05673acf83bab3580ee4fd3d4163ec4305
Sakthisa <sakthisa@oregonstate.edu>
2020-01-17 17:41:45 -0800

    Added DB functionality and comments

--- WindowConfiguration/Program.cs
+++ WindowConfiguration/Program.cs
@@ -9,13 +9,26 @@ using System.Collections;
 
 namespace WindowConfiguration
 {
+    public struct WindowInfo
+    {
+        public string Process_Name;
+        public string Process_Title;
+        public int Process_ID;
+        public int Left;
+        public int Right;
+        public int Top;
+        public int Bottom;
+        public int Width;
+        public int Height;
+    }
+
     static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         
-        /// Need these auxillary functions to pull in some window information
+        /// Since the standard C# library omits these window handler functions need to use import statements
         [DllImport("user32.dll", SetLastError = true)]
         static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
         [DllImport("user32.dll", SetLastError = true)]
@@ -24,6 +37,8 @@ namespace WindowConfiguration
         public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
         [DllImport("user32.dll")]
         public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
+
+
 
         //Struct passed into the window handler section which gives us the window position and size (after calculation)
         [StructLayout(LayoutKind.Sequential)]
@@ -37,8 +52,8 @@ namespace WindowConfiguration
         [STAThread]
         static void Main()
         {
-            // Get some information about all the screens
-            Console.WriteLine("\n");
+            // Gets information about all the monitors the current user has
+            Console.WriteLine("\n Monitor Information \n");
             foreach (var screen in Screen.AllScreens)
             {
                 Console.WriteLine("Device Name: " + screen.DeviceName);
@@ -50,16 +65,37 @@ namespace WindowConfiguration
             Console.WriteLine("\n");
 
 
+            // Some helper variables to get the process information and setting up rect/window handler
             Process[] processes = Process.GetProcesses();
             var count = 0;
             RECT rect = new RECT();
             IntPtr hWnd;
 
-            // Loop through all the currently running processes and get some diagnostic information for each window
+            // Here is how I am querying the database to get all the tuples. NOTE: will be empty initially until something is inserted
+            List<WindowInfo> window_lst = new List<WindowInfo>();
+            window_lst = SqLiteDataAccess.LoadWindow();
+            Console.WriteLine("\nFROM DATABASE\n");
+            foreach(var window in window_lst)
+            {
+                Console.WriteLine("DB Process ID: " + window.Process_ID);
+                Console.WriteLine("DB Process Name: " + window.Process_Name);
+                Console.WriteLine("DB Process Title: " + window.Process_Title);
+                Console.WriteLine("DB Left: " + window.Left);
+                Console.WriteLine("DB Right: " + window.Right);
+                Console.WriteLine("DB Top: " + window.Top);
+                Console.WriteLine("DB Bottom: " + window.Bottom);
+                Console.WriteLine("DB Width: " + window.Width);
+                Console.WriteLine("DB Height: " + window.Height + "\n");
+            }
+
+
+            // Loop through all the currently running processes and get some diagnostic information for each window. You can also insert into the DB and move the window (uncomment last few statements)
             foreach (var proc in processes)
             {
                 if (!string.IsNullOrEmpty(proc.MainWindowTitle))
                 {
+                    // Print out some diagnostic information
+                    WindowInfo window = new WindowInfo();
                     Console.WriteLine(++count + ".");
                     Console.WriteLine("Process Name: " + proc.ProcessName);
                     Console.WriteLine("Process Title: " + proc.MainWindowTitle);
@@ -72,10 +108,24 @@ namespace WindowConfiguration
                     Console.WriteLine("Window Height: " + Math.Abs((rect.Top) - (rect.Bottom)));
                     Console.WriteLine("\n");
 
-                    // Change window position and size of all foreground window applications to the top left.
+                    // Fill window struct in order to easily send an object to the sql insert function
+                    window.Process_Name = proc.ProcessName.ToString();
+                    window.Left = rect.Left;
+                    window.Right = rect.Right;
+                    window.Top = rect.Top;
+                    window.Bottom = rect.Bottom;
+                    window.Width = Math.Abs((rect.Left) - (rect.Right));
+                    window.Height = Math.Abs((rect.Top) - (rect.Bottom));
+                    window.Process_Title = proc.MainWindowTitle;
+                    window.Process_ID = proc.Id;
+
+                    // Uncomment the line below to insert all window handlers opened into the DB
+                    //SqLiteDataAccess.SaveWindow(window);
+
+
+                    // Uncomment the line below to move window handlers. NOTE: this only works for windows that are not minimized as far as I can tell.
                     // SetWindowPos(hWnd, 0, 0, 0, 500, 500, 0);
                 }
-
             }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
---
+++ WindowConfiguration/SqLiteDataAccess.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using Dapper;
+
+
+// Followed this tutorial on setting up the DB: https://www.youtube.com/watch?v=ayp3tHEkRc0
+
+namespace WindowConfiguration
+{
+    public class SqLiteDataAccess
+    {
+
+        // Query the database and return all tuples
+        public static List<WindowInfo> LoadWindow()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<WindowInfo>("select * from WindowHandler", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+
+
+        // Insert window handler information into the DB
+        public static void SaveWindow(WindowInfo window)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                int process_id = window.Process_ID;
+                string process_title = "'" + window.Process_Title + "'";
+                string process_name = "'" + window.Process_Name + "'";
+                int left = window.Left;
+                int right = window.Right;
+                int top = window.Top;
+                int bottom = window.Bottom;
+                int width = window.Width;
+                int height = window.Height;
+
+                string Sql_Insert = "insert into WindowHandler (Process_ID, Process_Title, Process_Name, Left, Right, Top, Bottom, Width, Height) values (" + process_id + ", " +process_title + ", "+process_name+"," +left+ ", " + right + ", " + top + ", " + bottom + ", " + width + ", " + height + ")";
+                //cnn.Execute("insert into WindowHandler (Process_Name, Left, Right, Top, Bottom, Width, Height) values (@Process_Name, @Left, @Right, @Top, @Bottom, @Width, @Height)", window);
+                cnn.Execute(Sql_Insert, window);
+            }
+        }
+
+
+        // Used to connect to the DB
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
